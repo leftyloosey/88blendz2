19539f310152df05635e8a14e1b22c4fd3163216
davidhardin <thisgoestodavid@gmail.com>
2022-12-08 20:57:07 -0700

    more

--- client/src/components/Merch.js
+++ client/src/components/Merch.js
@@ -1,7 +1,27 @@
+import product1 from '../images/image-hero.jpg'
+import product2 from '../images/fax.webp'
+
+const pics = [product1, product2, product1]
+
 const Merch = () => {
   return (
-    <div>
-      <h1>Merch</h1>
+    <div className='flex flex-col justify-center p-2 items-center md:flex-wrap '>
+      <div className='w-3/4 h-auto space-y-3'>
+        {pics.map((pic) => (
+          <div>
+            <img
+              src={pic}
+              key={pic.indexOf}
+              alt=''
+              className='gals shadow-lg outline'
+            />
+            <div className='flex flex-row justify-around text-[#552583] bg-[#FDB927] rounded-full opacity-70'>
+              <span>item name</span>
+              <span>$ $ $</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
