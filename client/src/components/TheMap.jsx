1df5f5011a566e418d44245df84af17ab696fa83
davidhardin <thisgoestodavid@gmail.com>
2025-01-05 12:32:04 -0700

    clickable red button

--- client/src/components/TheMap.jsx
+++ client/src/components/TheMap.jsx
@@ -1,28 +1,38 @@
-// import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps'
-import React from 'react'
-// import React, { useEffect, useRef } from 'react'
-// import { AdvancedMarker } from '@googlemaps/marker'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps'
+// import React from 'react'
+import React, { useEffect, useRef } from 'react'
+// import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+// const { AdvancedMarkerElement, PinElement } =
+//   await window.google.maps.importLibrary('marker')
 
 const HOBART = 'AIzaSyC0zTkwcKzG9r-sF6Xwtj3-WDgwSNDwdZ4'
 const id = '298edbd82e7e7d1a'
 const lat = 40.713177524497084
 const lng = -111.88789891349388
 
-// const TheMap = () => (
-//   <APIProvider apiKey={HOBART}>
-//     <Map
-//       mapId={'298edbd82e7e7d1a'}
-//       className='mt-12 rounded h-96 w-full'
-//       zoom={16}
-//       center={{ lat, lng }}
-//       gestureHandling={'greedy'}
-//       disableDefaultUI={true}
-//     >
-//       <AdvancedMarker position={{ lat, lng }} />
-//     </Map>
-//   </APIProvider>
-// )
+const TheMap = () => (
+  <APIProvider apiKey={HOBART}>
+    <Map
+      mapId={id}
+      className='mt-4 rounded h-96 w-full'
+      zoom={16}
+      center={{ lat, lng }}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    >
+      <AdvancedMarker
+        position={{ lat, lng }}
+        gmpClickable={true}
+        onClick={() => {
+          window.open(
+            'https://www.google.com/maps/dir//2641+S+State+St+Suite+B,+Salt+Lake+City,+UT+84115/@40.7130787,-111.9703319,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x87528bd13941b6e1:0xb1aa90e5f9f8f950!2m2!1d-111.8880075!2d40.7131082?entry=ttu&g_ep=EgoyMDI1MDEwMS4wIKXMDSoASAFQAw%3D%3D',
+            '_blank'
+          )
+        }}
+      />
+    </Map>
+  </APIProvider>
+)
 
 // const TheMap = () => {
 //   const mapRef = useRef(null)
@@ -30,7 +40,7 @@ const lng = -111.88789891349388
 //   useEffect(() => {
 //     const loader = new Loader({
 //       apiKey: HOBART,
-//       // libraries: ["places"], // Add any additional libraries you need
+//       libraries: ['markers'], // Add any additional libraries you need
 //     })
 
 //     loader.importLibrary().then(() => {
@@ -39,61 +49,62 @@ const lng = -111.88789891349388
 //         center: { lat: lat, lng: lng }, // Replace with your desired center
 //         zoom: 12,
 //       })
+
 //     })
 //   }, [])
 
 //   return <div ref={mapRef} style={{ height: '400px' }} />
 // }
 
-const containerStyle = {
-  width: '1600px',
-  height: '400px',
-}
+// const containerStyle = {
+//   width: '1600px',
+//   height: '400px',
+// }
 
-const center = {
-  lat: lat,
-  lng: lng,
-}
+// const center = {
+//   lat: lat,
+//   lng: lng,
+// }
 
-const TheMap = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: id,
-    googleMapsApiKey: HOBART,
-  })
+// const TheMap = () => {
+//   const { isLoaded } = useJsApiLoader({
+//     id: id,
+//     googleMapsApiKey: HOBART,
+//   })
 
-  const [map, setMap] = React.useState(null)
+//   const [map, setMap] = React.useState(null)
 
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    // const bounds = new window.google.maps.LatLngBounds(center)
-    // map.fitBounds(bounds)
+//   const onLoad = React.useCallback(function callback(map) {
+//     // This is just an example of getting and using the map instance!!! don't just blindly copy!
+//     // const bounds = new window.google.maps.LatLngBounds(center)
+//     // map.fitBounds(bounds)
 
-    setMap(map)
-  }, [])
+//     setMap(map)
+//   }, [])
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+//   const onUnmount = React.useCallback(function callback(map) {
+//     setMap(null)
+//   }, [])
 
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={16}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <Marker position={{ lat, lng }} />
+//   return isLoaded ? (
+//     <GoogleMap
+//       mapContainerStyle={containerStyle}
+//       center={center}
+//       zoom={16}
+//       onLoad={onLoad}
+//       onUnmount={onUnmount}
+//     >
+//       {/* Child components, such as markers, info windows, etc. */}
+//       <Marker position={{ lat, lng }} />
 
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
-  )
-}
+//       <></>
+//     </GoogleMap>
+//   ) : (
+//     <></>
+//   )
+// }
 
-export default React.memo(TheMap)
+// export default React.memo(TheMap)
 // const TheMap = () => {
 //   const { isLoaded } = useJsApiLoader({
 //     id: id,
@@ -117,4 +128,4 @@ export default React.memo(TheMap)
 //   )
 // }
 
-// export default TheMap
+export default TheMap
--- client/src/pages/Practice2.jsx
+++ client/src/pages/Practice2.jsx
@@ -14,8 +14,10 @@ import img4 from '../assets/images/gallery/img4.jpeg'
 import img6 from '../assets/images/gallery/img6.jpeg'
 import img7 from '../assets/images/gallery/img7.jpeg'
 import img8 from '../assets/images/gallery/img8.jpeg'
+import React, { Suspense } from 'react'
 
-import TheMap from '../components/TheMap'
+// import TheMap from '../components/TheMap'
+const TheMap = React.lazy(() => import('../components/TheMap'))
 
 const blendLink =
   'https://booksy.com/en-us/781756_88blendz-studio_barber-shop_37796_salt-lake-city#ba_s=vl_1'
@@ -102,8 +104,13 @@ const Practice2 = () => {
               </Link>
             </p>
           </div>
-          <div className='mt-4'>
-            <TheMap />
+          <div className=''>
+            {/* <TheMap /> */}
+            <div>
+              <Suspense fallback={<div>Loading...</div>}>
+                <TheMap />
+              </Suspense>
+            </div>
           </div>
         </div>
       </div>
