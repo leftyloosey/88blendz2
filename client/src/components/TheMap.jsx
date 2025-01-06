3effe36d1fd9e38f7ed687f6f275bb929b7e06d7
davidhardin <thisgoestodavid@gmail.com>
2025-01-06 01:46:40 -0700

    ap

--- client/src/components/TheMap.jsx
+++ client/src/components/TheMap.jsx
@@ -1,6 +1,7 @@
 import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps'
 import React, { useEffect, useRef } from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import { Loader } from '@googlemaps/js-api-loader'
 
 const HOBART = 'AIzaSyC0zTkwcKzG9r-sF6Xwtj3-WDgwSNDwdZ4'
 const id = '298edbd82e7e7d1a'
@@ -23,8 +24,11 @@ const center = {
 //       className='mt-4 rounded h-96 w-full'
 //       zoom={15}
 //       center={{ lat, lng }}
-//       gestureHandling={'cooperative'}
-//       // disableDefaultUI={true}
+//       // gestureHandling={'cooperative'}
+//       // disableDefaultUI={true}\
+// onCameraChanged={ (ev: MapCameraChangedEvent) =>
+//   console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+// }>
 //     >
 //       <AdvancedMarker
 //         // key={}
@@ -41,27 +45,36 @@ const center = {
 //   </APIProvider>
 // )
 // export default TheMap
-// const TheMap = () => {
-//   const mapRef = useRef(null)
 
-//   useEffect(() => {
-//     const loader = new Loader({
-//       apiKey: HOBART,
-//       libraries: ['markers'], // Add any additional libraries you need
-//     })
+const TheMap = () => {
+  const mapRef = useRef(null)
 
-//     loader.importLibrary().then(() => {
-//       const google = window.google
-//       const map = new google.maps.Map(mapRef.current, {
-//         center: { lat: lat, lng: lng }, // Replace with your desired center
-//         zoom: 12,
-//       })
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: HOBART,
+      version: 'weekly',
+    })
 
-//     })
-//   }, [])
+    loader.importLibrary('marker').then(() => {
+      const google = window.google
+      const map = new google.maps.Map(mapRef.current, {
+        center: { lat: lat, lng: lng }, // Replace with your desired center
+        zoom: 12,
+        mapId: id,
+      })
+      const AdvancedMarkerElement = google.maps.marker.AdvancedMarkerElement
+      // const { AdvancedMarkerElement } = google.maps.importLibrary('marker')
 
-//   return <div ref={mapRef} style={{ height: '400px' }} />
-// }
+      const marker = new AdvancedMarkerElement({
+        // id: id,
+        map,
+        position: { lat: lat, lng: lng },
+      })
+    })
+  }, [])
+
+  return <div ref={mapRef} style={{ height: '400px' }} />
+}
 
 // const TheMap = () => {
 //   const { isLoaded } = useJsApiLoader({
@@ -100,66 +113,5 @@ const center = {
 //     <></>
 //   )
 // }
-
-// export default React.memo(TheMap)
-const TheMap = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: id,
-    googleMapsApiKey: HOBART,
-  })
-
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    // const bounds = new window.google.maps.LatLngBounds(center)
-    // map.fitBounds(bounds)
-
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-    <APIProvider apiKey={HOBART}>
-      <Map
-        mapId={id}
-        className='mt-4 rounded h-96 w-full'
-        zoom={15}
-        center={{ lat, lng }}
-        gestureHandling={'cooperative'}
-        // disableDefaultUI={true}
-      >
-        <AdvancedMarker
-          // key={}
-          position={{ lat, lng }}
-          gmpClickable={true}
-          onClick={() => {
-            window.open(
-              'https://www.google.com/maps/dir//2641+S+State+St+Suite+B,+Salt+Lake+City,+UT+84115/@40.7130787,-111.9703319,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x87528bd13941b6e1:0xb1aa90e5f9f8f950!2m2!1d-111.8880075!2d40.7131082?entry=ttu&g_ep=EgoyMDI1MDEwMS4wIKXMDSoASAFQAw%3D%3D',
-              '_blank'
-            )
-          }}
-        />
-      </Map>
-    </APIProvider>
-  ) : (
-    // <GoogleMap
-    //   mapContainerStyle={containerStyle}
-    //   center={center}
-    //   zoom={16}
-    //   onLoad={onLoad}
-    //   onUnmount={onUnmount}
-    // >
-    //   {/* Child components, such as markers, info windows, etc. */}
-    //   <Marker position={{ lat, lng }} />
-
-    //   <></>
-    // </GoogleMap>
-    <></>
-  )
-}
 
 export default React.memo(TheMap)
