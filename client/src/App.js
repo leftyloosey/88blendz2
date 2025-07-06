0e1dc4600364a3c5481c31119f8b5584e7816a93
davidhardin <thisgoestodavid@gmail.com>
2025-07-06 16:57:47 +0300

    new favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import SignUpList from './pages/SignUpList'
 // import Home from './pages/Home'
-import Practice from './pages/Practice'
 import Practice2 from './pages/Practice2'
 import Merch from './pages/Merch'
 
@@ -9,17 +7,11 @@ function App() {
   return (
     <>
       <Routes>
-        {/* <Route path='/' element={<TheMap />} /> */}
-        {/* <Route path='/' element={<ReadImage />} /> */}
         <Route path='/' element={<Practice2 />} />
 
         {/* <Route index element={<Home />} /> */}
-        <Route path='/signuplist' element={<SignUpList />} />
-        <Route path='/practice' element={<Practice />} />
         <Route path='/practice2' element={<Practice2 />} />
         <Route path='/merch' element={<Merch />} />
-        {/* <Route path='*' element={<Home />} /> */}
-        {/* <Route path='*' element={<ReadImage />} /> */}
         <Route path='*' element={<Practice2 />} />
       </Routes>
     </>
--- client/src/pages/Practice.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-const Practice = () => {
-  const navigate = useNavigate()
-  return <></>
-}
-
-export default Practice
