48a0b6b856345e7d3507eb3816145c056b0d1519
davidhardin <thisgoestodavid@gmail.com>
2025-07-07 18:50:43 +0300

    deletions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-// import Home from './pages/Home'
 import Practice2 from './pages/Practice2'
 import Merch from './pages/Merch'
 
@@ -8,8 +7,6 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Practice2 />} />
-
-        {/* <Route index element={<Home />} /> */}
         <Route path='/practice2' element={<Practice2 />} />
         <Route path='/merch' element={<Merch />} />
         <Route path='*' element={<Practice2 />} />
